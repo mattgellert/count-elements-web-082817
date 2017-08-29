f05339639b4f84de738c514f9435253cb667dcb7
mattgellert <mattgellert@gmail.com>
2017-08-29 08:15:37 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
-def count_elements(array)
-  # code goes here
+def count_elements(arr)
+  hsh = {}
+
+  arr.each {|x|
+    if hsh.keys.include?(x)
+      hsh[x] += 1
+    else
+      hsh[x] = 1
+    end
+  }
+  hsh
 end
- 
